f8d1c84a2cbb4a59f939c9952a361e260ae70f78
Felipe Santana <felipe008_@hotmail.com>
2021-07-26 21:30:40 -0300

    feature: response com JSON

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const http = require('http')
 
 http.createServer((req, res) => {
+    // CONTEÚDO COMO TEXTO SIMPLES
         // res.writeHead(200, {
         //     'Content-Type': 'text/plain; charset=utf-8'
         // })
         // res.end('Servidor funcionando redondinho, ó!')
+        
+        // CONTEÚDO COMO HTML
+    //     res.writeHead(200, {
+    //         'Content-Type': 'text/html; charset=utf-8'
+    //     })
+    //     res.end('<h1>Servidor funcionando redondinho, ó!</h1>')
+    // }).listen(3000, 'localhost', () => console.log('O servidor está rodando...'))
+
+        // CONTEÚDO COMO JSON
         res.writeHead(200, {
-            'Content-Type': 'text/html; charset=utf-8'
+            'Content-Type': 'application ld/json; charset=utf-8'
         })
-        res.end('<h1>Servidor funcionando redondinho, ó!</h1>')
-    }).listen(3000, 'localhost', () => console.log('O servidor está rodando...'))
+        res.end('{"code": 200, "message": "servidor funcionando!"}')
+         }).listen(3000, 'localhost', () => console.log('O servidor está rodando...'))

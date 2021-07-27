655ca049282a59fae3013a2d436200c0d5e99b4d
Felipe Santana <felipe008_@hotmail.com>
2021-07-26 21:50:04 -0300

    Atualização das páginas  usando case

--- index.js
+++ index.js
@@ -16,7 +16,16 @@ http.createServer((req, res) => {
 
         // CONTEÚDO COMO JSON
         res.writeHead(200, {
-            'Content-Type': 'application ld/json; charset=utf-8'
-        })
-        res.end('{"code": 200, "message": "servidor funcionando!"}')
-         }).listen(3000, 'localhost', () => console.log('O servidor está rodando...'))
+            'Content-Type': 'text/html;charset=utf-8'
+          })
+          switch (req.url) {
+            case '/':
+              return res.end('<h1>VOCÊ ESTÁ NA HOME</h1>')
+            case '/sobre':
+              return res.end('<h1>VOCÊ ESTÁ NA PÁGINA SOBRE</h1>')
+            case '/produto':
+              return res.end('<h1>VOCÊ ESTÁ NA PÁGINA DE PRODUTO</h1>')
+            default:
+              return res.end(`<h1>NÃO EXISTE ESSA PÁGINA${req.url.toUpperCase().replace(/\/|-/g,' ')}</h1>`)
+          }
+     }).listen(3000, 'localhost', () => console.log('O servidor está rodando...'))

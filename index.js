f507a31e00ba60223af219057710fc275c89ef39
Felipe Santana <felipe008_@hotmail.com>
2021-07-26 21:19:29 -0300

    feature: definindo response Header com status 200 - OK e tipo de conteúdo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require('http')
 
-http.createServer(
-    (req, res) => {
-        res.end('Servidor funcionando')
+http.createServer((req, res) => {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        })
+        res.end('Servidor funcionando redondinho!')
     }).listen(3000, 'localhost', () => console.log('O servidor está rodando...'))

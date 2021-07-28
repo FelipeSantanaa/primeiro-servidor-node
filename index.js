229881caa61fb98b6db3b3a404703aab5976e49e
Felipe Santana <felipe008_@hotmail.com>
2021-07-28 20:55:53 -0300

    feature: rotas com parametros obrigatórios e opcionais

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
+const { response } = require('express')
+
 const express = require('express'),
   app = express()
 
+
+  //app.METODO_DO_EXPRESS(CAMINHO_OU_PATH, FUNCAO_HANDLER)
   app.get('/',(req, res) => {
-    res.send('VOCÊ ESTÁ NA HOME')
+    res.send('<h1>VOCÊ ESTÁ NA HOME</h1>')
   })
 
+  app.get('/sobre', (req, resp) => {
+    resp.send('<h1> VOCÊ ESTÁ NA PÁGINA SOBRE</h1>')
+  })
+
+  app.get('/produto/:id/:cor?', (req, res) => {
+    console.log(req.params)
+    if (req.params.cor) {
+      res.send(`<h1>VOCÊ ESTÁ NA PÁGINA DO PRODUTO DE ID ${req.params.id} E DE COR ${req.params.cor}</h1>`)
+    } else {
+      res.send(`<h1>VOCÊ ESTÁ NA PÁGINA DO PRODUTO DE ID ${req.params.id}</h1>`)
+    }
+  })
+  
+app.get('/usuarios/:nome?', (req, res) => {
+  const {nome} = req.params
+  nome ? res.send(`Página do usuário ${nome}`) : res.send('Página de usuários')
+})
+
+
+  app.get('/objeto', (req, resp) => {
+    resp.send({
+      nome: "Joselito",
+      sobrenome: "Sem Noção",
+      melhoresAmigos: "Jorgin e Buchecha"})
+  })
+
+  app.get('/json', (req, resp) => {
+    var frutas = ['banana, maça, uva, pera']
+    resp.send(frutas)
+  })
 app.listen(3000, () => console.log('O servidor está rodando...'))
